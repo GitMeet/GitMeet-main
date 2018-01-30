e9032c4b8097dc1c5edec7297717a0a6e6795a08
GitMeet <35901314+GitMeet@users.noreply.github.com>
2018-01-30 18:10:59 -0500

    Create remove_top_project.py

---
+++ recruitapp/remove_top_project.py
@@ -0,0 +1,14 @@
+import click
+import tigerSqlite
+
+@click.command()
+@click.option('--projectname')
+@click.option('--username')
+def remove_top_project(projectname, username):
+    if not any(a == projectname and b == username for a, b in tigerSqlite.Sqlite('top_projects.db').get_projectname_owner('best')):
+        raise ValueError('project "{}" with creater @{} not found'.format(projectname, username))
+    tigerSqlite.Sqlite('top_projects.db').delete('best', ('projectname', projectname))
+    click.echo('project removed')
+
+if __name__ == '__main__':
+    remove_top_project()
