35ac81b48ec07f71ec845a78415cb5ee6601ffa0
GitMeet <35901314+GitMeet@users.noreply.github.com>
2018-02-14 10:35:47 -0500

    Update joined_projects.py

--- recruitapp/joined_projects.py
+++ recruitapp/joined_projects.py
@@ -5,10 +5,14 @@ class Joined:
         self.__dict__ = dict(zip(['user', 'projectdb'], args))
         self.full_projects = [project.Project(i) for i in self.projectdb.get_projectname_description_owner_id_team_teamneeded_requests_extra('projects')]
         self.joined_projects = [i for i in self.full_projects if any(t.username == self.user for t in i.team)]
+        print "in error logs testing self.joined_projects", self.joined_projects
 
     @property
     def length(self):
-        return len(self.joined_projects)
+        count = 0
+        for project in self.joined_projects:
+            count += sum([len([b for b in project.jobs if b.id == i.id]) for i in project.team if i.username == self.user])
+        return count
 
     def __iter__(self):
         if not self.joined_projects:
@@ -17,6 +21,6 @@ class Joined:
         for project in self.joined_projects:
 
             specific_project = [[b for b in project.jobs if b.id == i.id][0] for i in project.team if i.username == self.user]
-            
+
             print "specific_project", specific_project
             yield joined_project(project, specific_project)
